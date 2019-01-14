ff375e04871433d4c7c5c6155648244149f4c46e
Anubhav <Anubhav@LAPTOP-J7552BDC>
2019-01-14 01:03:19 -0500

    REGISTRATION,LOGIN,USER PROFILE WEB SERVICES IS DONE

---
+++ src/java/lunch/GenericResource.java
@@ -0,0 +1,167 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lunch;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import net.sf.json.JSONObject;
+
+/**
+ * REST Web Service
+ *
+ * @author Anubhav
+ */
+@Path("application")
+public class GenericResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of GenericResource
+     */
+    public GenericResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of lunch.GenericResource
+     *
+     * @return an instance of java.lang.String
+     */
+    Connectionclass conclass = new Connectionclass();
+    Statement stm;
+
+    int number = 0;
+
+    @GET
+    @Path("registration&{firstName}&{lastName}&{email}&&{password}&{phonenumber}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJson(@PathParam("firstName") String fn, @PathParam("lastName") String ln, @PathParam("email") String email,
+            @PathParam("password") String pass,
+            @PathParam("phonenumber") String pNumber) throws SQLException {
+        System.out.println("111," + fn + "," + ln + "," + email + "," + pass + "," + pNumber + ");");
+        try {
+            stm = conclass.createConnection();
+            number = stm.executeUpdate("INSERT INTO USERS VALUES(111," + fn + "," + ln + "," + email + "," + pass + "," + pNumber + ")");
+            System.out.println("total inserted rows" + number);
+
+            stm.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(GenericResource.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "";
+    }
+
+    //defining and declaring a JSon object
+    JSONObject singledata = new JSONObject();
+
+    @GET
+    @Path("Login&{email}&{password}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJson(@PathParam("email") String email, @PathParam("password") String pass) {
+
+        stm = conclass.createConnection();
+
+        try {
+            System.out.println("select * from USERS WHERE email=" + email + " and userpassword=" + pass);
+            ResultSet rs = stm.executeQuery("select * from USERS WHERE username=" + email + " and userpassword=" + pass);
+
+            String fName, lName, contactnumber, userpassword, dateOfbirth;
+
+            int user_Id;
+
+            while (rs.next()) {
+                System.out.println("yaaehhhhhhhhhhhhhhhhhhhhhhn........................................");
+                fName = rs.getString("FIRSTNAME");
+                lName = rs.getString("LASTNAME");
+                email = rs.getString("EMAIL");
+                contactnumber = rs.getString("PHONE");
+                userpassword = rs.getString("PASSWORD");
+                user_Id = rs.getInt("USER_ID");
+                System.out.println("username is " + fName);
+
+                singledata.accumulate("Status", "OK");
+
+                singledata.accumulate("Active", true);
+                singledata.accumulate("user_id", user_Id);
+
+            }
+
+            rs.close();
+            stm.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(GenericResource.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return singledata.toString();
+    }
+
+    
+    
+    @GET
+    @Path("myprofile&{userid}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJson(@PathParam("userid") int u_id) {
+
+        stm = conclass.createConnection();
+        try {
+            System.out.println("select * from USERS WHERE user_id=" + u_id );
+            ResultSet rs = stm.executeQuery("select * from USERS WHERE USER_ID=" + u_id );
+
+            String fName, lName, email, contactnumber, userpassword, username;
+
+            int user_Id;
+
+            while (rs.next()) {
+                System.out.println("rs have values........................");
+                fName = rs.getString("FIRSTNAME");
+                username=rs.getString("USERNAME");
+                lName = rs.getString("LASTNAME");
+                email = rs.getString("EMAIL");
+                contactnumber = rs.getString("PHONE");
+                userpassword = rs.getString("PASSWORD");
+                user_Id=rs.getInt("USER_ID");
+                System.out.println("username is " + fName);
+
+                singledata.accumulate("Status", "OK");
+                singledata.accumulate("username",username );
+                singledata.accumulate("firstname",fName );
+                singledata.accumulate("lastname",lName );
+                singledata.accumulate("email",email );
+                singledata.accumulate("contactnumber", contactnumber);
+                singledata.accumulate("userpassword",userpassword );
+                singledata.accumulate("user_id", user_Id);
+                singledata.accumulate("Active", true);
+                
+
+            }
+
+            rs.close();
+            stm.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(GenericResource.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return singledata.toString();
+    }
+    
+    
+
+}
